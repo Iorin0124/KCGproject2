39bcecaf9cf216e8255d649655b16a95d436d35e
Sakurako Yamamoto <sakura01240305@gmail.com>
2017-11-29 19:08:13 +0900

    とりあえず北海道と青森の検索フィールドまで。 書き方の模索中。

--- resources/views/lay-master.blade.php
+++ resources/views/lay-master.blade.php
@@ -4,6 +4,7 @@
       <meta charset="utf-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1">
+      <script type="text/javascript" src="jquery-2.1.3.js"></script>
 
       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -4,42 +4,27 @@
 
 @section('content')
 <!--　決まり文句　-->
-  <div><center>
-    <p class="largeFont">『<strong>V</strong>oice <strong>a</strong>cter <strong>A</strong>nime <strong>p</strong>roject』通称<strong>VaAp</strong>へようこそ</p>
-    <p class="largeFont linedotted"><strong>VaAp</strong>はあなたの快適なアニメライフを応援します</p>
-  </center></div>
+
 
 <!--　検索に必要な条件の設定フォーム　-->
     <p class="btnicon-Search largeFont padl">現在放送中のアニメを探す</p>
   <div>
     <!--　都道府県選択　-->
     <form class="padl-2">
-      <select class="middleFont puldown-1" name="都道府県">
-        <option value="0">北海道</option><option value="1">青森</option><option value="2">岩手県</option>
-        <option value="3">宮城県</option><option value="4">秋田県</option><option value="5">山形県</option>
-        <option value="6">福島県</option><option value="7">茨城県</option><option value="8">栃木県</option>
-        <option value="9">群馬県</option><option value="10">埼玉県</option><option value="11">千葉県</option>
-        <option value="12">東京都</option><option value="13">神奈川県</option><option value="14">新潟県</option>
-        <option value="15">富山県</option><option value="16">石川県</option><option value="17">福井県</option>
-        <option value="18">山梨県</option><option value="19">長野県</option><option value="20">岐阜県</option>
-        <option value="21">静岡県</option><option value="22">愛知県</option><option value="23">三重県</option>
-        <option value="24">滋賀県</option><option value="25">京都府</option><option value="26">大阪府</option>
-        <option value="27">兵庫県</option><option value="28">奈良県</option><option value="29">和歌山県</option>
-        <option value="30">鳥取県</option><option value="31">島根県</option><option value="32">岡山県</option>
-        <option value="33">広島県</option><option value="34">山口県</option><option value="35">徳島県</option>
-        <option value="36">香川県</option><option value="37">愛媛県</option><option value="38">高知県</option>
-        <option value="39">福岡県</option><option value="40">佐賀県</option><option value="41">長野県</option>
-        <option value="42">熊本県</option><option value="43">大分県</option><option value="44">宮崎県</option>
-        <option value="45">鹿児島県</option><option value="46">沖縄県</option>
+      <select class="middleFont puldown-1" name="都道府県" id="todohuken">
+        @foreach(config("todohuken") as $index => $name)
+          <option value="{{$index}}">{{$name}}</option>
+        @endforeach
       </select>
       <i class="padl-2" />
+
       <!--　放送ステータス　-->
-      <select class="middleFont puldown-1" name="ステータス">
-        <option value="0">放送中</option><option value="1">新規</option>
+      <select class="middleFont puldown-2" name="ステータス" id="status">
+        <option value="0">都道府県を選択</option>
       </select>
       <i class="padl-2" />
       <!--　ソート条件　-->
-      <select class="middleFont puldown-2" name="ソート順">
+      <select class="middleFont puldown-2" name="ソート順" id="soat">
         <option value="0">今日の放送</option><option value="1">放送曜日順</option><option value="2">五十音順</option>
       </select>
       <i class="padl-2" />
@@ -48,6 +33,26 @@
     </form>
   </div>
 <!--　フォーム終了　-->
+
+<!--　どの都道府県が選択されるかにより選択できるICを変える　-->
+<script type="text/javascript">
+  $("#todohuken").change(function(){
+    var todonum = $(this).val();
+    //console.log(todonum);   //どの都道府県が選択されているか番号で取得、確認
+    $("#status").empty();
+    if(todonum == "1"){   //北海道
+      @foreach(config("ic_hokkaido") as $index => $name)
+        $("#status").append($("<option>").val("{{$index}}").text("{{$name}}"));
+      @endforeach
+    }else if(todonum == "2"){   //青森県
+      @foreach(config("ic_aomori") as $index => $name)
+        $("#status").append($("<option>").val("{{$index}}").text("{{$name}}"));
+      @endforeach
+    }
+  });
+</script>
+
+
 
 <!--　div内に検索した番組情報を表示する　フェッチに書き換えてくれてok　-->
   <div class="padl-2">
