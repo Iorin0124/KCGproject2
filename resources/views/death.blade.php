f172f2793eea2c8b01437dce406c045d1785ea59
Sakurako Yamamoto <sakura01240305@gmail.com>
2017-12-03 20:03:13 +0900

    レイアウト調整

--- resources/views/death.blade.php
+++ resources/views/death.blade.php
@@ -4,15 +4,18 @@
 
 
 @section('content')
-  <h1 class="padl-2"><strong>没作品の墓場</strong></h1>
-  <div class="padl-2">
-    <p class="middleFont">↓トップページに戻るリンク用の画像アイコン、企画破綻で没。</p>
-    <!-- <p class="middleFont">作画:西田 絢　　編集:山本 桜子</p> -->
-    <img src="../img/topIcon01.png" alt="topicon"  height="80px;" width="290px;">
-    <p class="padt middleFont">↓トップページ掲載用のガソリンスタンド画像、企画破綻で没。</p>
-    <!-- <p class="middleFont">作画:西田 絢　　編集:山本 桜子</p> -->
-    <img src="../img/gstand.png" alt="gstarnd" width="900px">
-    <br><br><br>
+  <div  class="padl">
+    <h1><strong>没作品の墓場</strong></h1>
+    <div class="padl">
+      <p class="middleFont">↓トップページに戻るリンク用の画像アイコン、企画破綻で没。</p>
+      <!-- <p class="middleFont">作画:西田 絢　　編集:山本 桜子</p> -->
+      <img src="../img/topIcon01.png" alt="topicon"  height="80px;" width="290px;">
+      <p class="padt middleFont">↓トップページ掲載用のガソリンスタンド画像、企画破綻で没。</p>
+      <!-- <p class="middleFont">作画:西田 絢　　編集:山本 桜子</p> -->
+      <img src="../img/gstand.png" alt="gstarnd" width="900px">
+
+      <br><i class="pad"></i>
+    </div>
   </div>
 
 @endsection
