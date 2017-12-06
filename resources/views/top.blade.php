793f7e30594f1369d62e87e7c49df9792914ca1d
Sakurako Yamamoto <sakura01240305@gmail.com>
2017-12-06 12:10:06 +0900

    軽度の調整

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -8,20 +8,20 @@
 
 <!--　検索に必要な条件の設定フォーム　-->
   <p class="btnicon-Search largeFont padl">ICからルート検索</p>
-  <!--　出発　都道府県選択　※idをstartPrefに変えたい　-->
+  <!--　出発　都道府県選択　-->
   <div>
   <form class="padl-2" action="{{route('top/')}}" method="POST">
     {{csrf_field()}}
     <p class="inline middleFont"><strong>出発地</strong></p>
-      <select class="middleFont puldown-1" name="startP" id="todohuken">
+      <select class="middleFont puldown-1" name="startP" id="startPref">
         @foreach(config("pref") as $index => $name)
           <option name="pref" value="{{$index}}">{{$name}}</option>
         @endforeach
       </select>
       <i class="smallpad"></i>
 
-  <!--　出発　IC名選択　※idをstartIcに変えたい　-->
-  <select class="middleFont puldown-2" name="inIC" id="status">
+  <!--　出発　IC名選択　-->
+  <select class="middleFont puldown-2" name="inIC" id="inIc">
     @foreach(config('ic') as $index => $i)
       @foreach($i as $name => $n)
         @if($index==1)
@@ -32,19 +32,17 @@
   </select>
   <i class="smallpad btnicon-ArrowRight"></i>
 
-  <!--　到着　都道府県選択　※まだPOSTが機能してない　-->
-<!--  <form class="" action="{{route('top/')}}" method="POST">  -->
-    {{csrf_field()}}
+  <!--　到着　都道府県選択　-->
     <p class="inline middleFont"><strong>到着地</strong></p>
-      <select class="middleFont puldown-1" name="endP" id="endPref">
+      <select class="middleFont puldown-1" name="goalP" id="goalPref">
         @foreach(config("pref") as $index => $name)
           <option name="pref" value="{{$index}}">{{$name}}</option>
         @endforeach
       </select>
       <i class="smallpad"></i>
 
-  <!--　到着　IC名選択　※まだPOSTが機能してない　-->
-  <select class="middleFont puldown-2" name="endIC" id="endIc">
+  <!--　到着　IC名選択　-->
+  <select class="middleFont puldown-2" name="outIC" id="outIc">
     @foreach(config('ic') as $index => $i)
       @foreach($i as $name => $n)
         @if($index==1)
@@ -60,7 +58,7 @@
   <!--　車種　-->
   <div class="padt padl-2">
     <p class="inline middleFont"><strong>車種選択</strong></p>
-    <select class="middleFont puldown-1" name="carM" id="carModel">
+    <select class="middleFont puldown-1" name="car" id="carModel">
       <option value="0">普通車</option><option value="1">軽自動車</option><option value="2">中型車</option>
       <option value="3">大型車</option><option value="4">特大車</option>
     </select>
@@ -77,20 +75,19 @@
   <button type="submit" class="btnicon-MousePoint middleFont puldown-1" name="button">検索</button>
   </form>
 </div>
-
 <!--　フォーム終了　-->
 
 <!--　どの都道府県が選択されるかにより選択できるICを変える　-->
 <script type="text/javascript">
-  $("#todohuken").change(function(){
-    var todonum = $(this).val();
-    //console.log(todonum);   //どの都道府県が選択されているか番号で取得、確認
-	$("#status").empty();
+  $("#startPref").change(function(){
+    var prefnum = $(this).val();
+    //console.log(prefnum);   //どの都道府県が選択されているか番号で取得、確認
+	$("#inIc").empty();
 	    @foreach(config('ic') as $index => $i)
 		var index = <?php echo json_encode($index, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT); ?>;
 			@foreach($i as $name => $n)
-				if(todonum==index){
-					$("#status").append($("<option>").val("{{$name}}").text("{{$n}}"));
+				if(prefnum==index){
+					$("#inIc").append($("<option>").val("{{$name}}").text("{{$n}}"));
 				}
 			@endforeach
 		@endforeach
