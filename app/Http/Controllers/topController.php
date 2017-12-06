80ec4222d8c9f445de38a6e2a6f14929fe94f5d3
Sakurako Yamamoto <sakura01240305@gmail.com>
2017-12-06 15:57:27 +0900

    Revert "衝突の回避の為mcs/07の必要な内容のみここにプッシュする"
    This reverts commit ab08a88d5fd9b9fa350670c6d07bcad83d635a32.

--- app/Http/Controllers/topController.php
+++ app/Http/Controllers/topController.php
@@ -9,23 +9,20 @@ class topController extends Controller
     public function search(Request $request){
 		$startP = $request->input('startP',1);
 		$inIC = $request->input('inIC',1);
-
-		$goalP = $request->input('goalP',1);
-		$outIC = $request->input('outIC',1);
-		$car = $request->input('car','普通車');
+		
+		//$goalP = $request->input('goalP',1);
+		//$outIC = $request->input('outIC',1);
 		$sort = $request->input('sort',0);
-
-		//IC名の取得
+		
 		foreach(config('ic') as $index => $i){
 			foreach($i as $name => $n){
 				if($index==$startP && $name==$inIC){
 					$inIC = $n;
-				}else if($index==$goalP && $name==$outIC){
-					$outIC = $n;
+					break;
 				}
 			}
 		}
-		$url = 'http://kousoku.jp/api/route.php?f='.$inIC.'&t='.$goalIC.'&c='.$car.'&sortBy='.$sort;
+//		$url = 'http://kousoku.jp/api/route.php?f=';
 //		$array = simplexml_load_string(file_get_contents($choice));
 //		$item = $array->response->item;
 
