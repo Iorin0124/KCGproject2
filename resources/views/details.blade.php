5b77b9e51442942d9c29e16ba9a1ef46a6d9f833
Sakurako Yamamoto <sakura01240305@gmail.com>
2018-01-18 11:11:08 +0900

    素材画像用意と新ページ追加

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -7,13 +7,13 @@
 
   <div class="padl-2" style="overflow:hidden">
     <div class="headcol img-div" style="float:left;">
-      <img src="" alt="経路地図" style="width:700px; height=600px;">
+      <img src="../img/kari.jpg" alt="経路地図" style="width:700px; height=600px;">
     </div>
     <div  style="float:right;">
       <table class="tablespace-2">
-        <tr><th >a</th><th>a</th><th>a</th></tr>
-        <tr><td>b</td><td>b</td><td>b</td></tr>
-        <tr><td>b</td><td>b</td><td>b</td></tr>
+        <tr><th >移動経路</th><th>使用道路</th><th>距離と時間</th></tr>
+        <tr><td>b<br />↓</td><td>b</td><td>b</td></tr>
+        <tr><td>b<br />↓</td><td>b</td><td>b</td></tr>
       </table>
     </div>
   </div>
