40e445c5db88dbbdc03a5c192f92e9b69c566600
Sakurako Yamamoto <sakura01240305@gmail.com>
2018-01-30 16:01:02 +0900

    データ入力

--- app/Http/Controllers/DetailController.php
+++ app/Http/Controllers/DetailController.php
@@ -16,12 +16,48 @@ class DetailController extends Controller
 		}else{
 			$item("aaa");
 		}
-		
+
 		$highway = ["東名高速道路","中央高速道路","名神高速道路","東名神高速道路","東名神高速道路","伊勢湾岸自動車道","常盤自動車道","磐越自動車道","東北自動車道","秋田自動車道","関越自動車道","中国自動車道","山陽自動車道","山陽自動車道","山陽自動車","山陽自動車道","山陽自動車道","米子自動車道","九州自動車道","大分自動車道","大分自動車道","長崎自動車道","宮崎自動車道","北陸自動車道","上信越自動車道","東海北陸自動車道","東海北陸自動車道","道央自動車道","札幌自動車道","道東自動車道","道東自動車道","道東自動車","松山自動車道","松山自動車道","高松自動車道","高松自動車道","徳島自動車道","高知自動車道"];
-		
+
 		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-toumei.json";
 		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-chuou.json";
 		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-meishin.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-higashimeihan1.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-higashimeihan2.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-isewangan.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-jyouban.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-banetsu.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-touhoku.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-akita.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-kanetsu.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-chugoku.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-sanyou1.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-sanyou2.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-sanyou3.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-sanyou4.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-sanyou5.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-yonago.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-kyushu.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-ooita1.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-ooita2.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-nagasaki.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-miyazaki.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-hokuriku.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-jyoshinetsu.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-toukaihokuriku1.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-toukaihokuriku2.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-douou.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-sasson.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-doutou1.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-doutou2.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-doutou3.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-matsuyama1.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-matsuyama2.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-takamatsu1.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-takamatsu2.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-tokushima.json";
+		$url[] = "https://www.ajaxtower.jp/googlemaps/data/data/highway-kochi.json";
+
 		//$url[]にJSONのリンクを追加する。(すべて)
 		for($i=0 ; $i<count($url) ; $i++){
 			$json = file_get_contents($url[$i]);
@@ -34,7 +70,7 @@ class DetailController extends Controller
 				$result[$highway[$i]][$j][] = $arr['marker'][$j]['lng'];
 			}
 		};
-		
+
 		return view('details',compact('result','item'));
 	}
 }
