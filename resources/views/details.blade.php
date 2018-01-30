98ac0f22fc11cff25a9812989277473801b2440d
Sakurako Yamamoto <sakura01240305@gmail.com>
2018-01-30 15:58:24 +0900

    作業途中　保存の為コミット

--- resources/views/details.blade.php
+++ resources/views/details.blade.php
@@ -7,7 +7,10 @@
 
   <div class="padl-2" style="overflow:hidden">
     <div class="headcol img-div" style="float:left;">
-      <img src="../img/kari.jpg" alt="経路地図" style="width:700px; height=600px;">
+      <img src="" alt="経路地図" style="width:700px; height=600px;">
+      <!-- dj00aiZpPU8wNHp2UFFsNkkzWiZzPWNvbnN1bWVyc2VjcmV0Jng9YWM- -->
+      <!-- サンプルURL https://map.yahooapis.jp/course/V1/routeMap?appid=dj00aiZpPU8wNHp2UFFsNkkzWiZzPWNvbnN1bWVyc2VjcmV0Jng9YWM-&route=35.6659127870595,139.7308921534391,35.6631583463388,139.731235476193&pin=35.6659127870595,139.7308921534391&pin=35.6631583463388,139.731235476193|width:7|color:ff4500&width=700&height=600&style=grayish -->
+      <!-- サイズ以外のコマンドが効いていないので要確認 -->
     </div>
     <div  style="float:right;">
       <table class="tablespace-2">
